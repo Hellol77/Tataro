5100a39acbe8d1130e15ed70bdd90dfe68fea53f
Hellol77 <dhe7700@naver.com>
2023-05-25 03:21:06 +0900

    fix: 카카오톡 공유하기 수정

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -154,3 +154,13 @@ async function getTaro() {
     console.log(error);
   }
 }
+
+function shareMessage() {
+  Kakao.Share.sendCustom({
+    templateId: 94226,
+    templateArgs: {
+      title: myFirstQuestion,
+      description: tataroFirstAnswer,
+    },
+  });
+}
--- frontend/js/kakaoShare.js
+++
@@ -1,9 +0,0 @@
-function shareMessage() {
-  Kakao.Share.sendCustom({
-    templateId: 94226,
-    templateArgs: {
-      title: myFirstQuestion,
-      description: tataroFirstAnswer,
-    },
-  });
-}
