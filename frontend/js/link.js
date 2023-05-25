fac0c4bb306e7b0d0c9540fdffdf740f2384328f
Hellol77 <dhe7700@naver.com>
2023-05-25 17:01:51 +0900

    fix: input 제출 버그 수정

--- frontend/js/link.js
+++ frontend/js/link.js
@@ -2,17 +2,22 @@
 const startPage = document.getElementById("start_page");
 const startButton = document.getElementById("first_button");
 const insertPage = document.getElementById("insert_page");
+const insertValue = document.getElementById("insert_text_input_value");
 const insertButton = document.getElementById("second_button");
 const chooseCardPage = document.getElementById("chooseCard_page");
 const chooseCardButton = document.getElementById("chooseCard_select");
 const cardResultPage = document.getElementById("cardResult_page");
 const cardResultButton = document.getElementById("result");
 const totalResultPage = document.getElementById("totalResult_page");
+const totalResultButton = document.getElementById("totalResult_Button");
 const totalResultShareButton = document.getElementById(
   "totalResultShare_button"
 );
 const totalResultResumeButton = document.getElementById(
   "totalResultResume_button"
+);
+const totalResultInputValue = document.getElementById(
+  "totalResult_input_value"
 );
 const shareButtonReload = document.getElementById("sharePageButtonReload");
 const sharePage = document.getElementById("share_page");
@@ -21,9 +26,14 @@ startButton.addEventListener("click", () => {
   startPage.style.display = "none";
   insertPage.style.display = "flex";
 });
-insertButton.addEventListener("click", () => {
-  insertPage.style.display = "none";
-  chooseCardPage.style.display = "flex";
+insertButton.addEventListener("click", (e) => {
+  if (insertValue.value == "") {
+    return;
+  } else {
+    getTaro();
+    insertPage.style.display = "none";
+    chooseCardPage.style.display = "flex";
+  }
 });
 chooseCardButton.addEventListener("click", () => {
   chooseCardPage.style.display = "none";
@@ -40,12 +50,32 @@ cardResultButton.addEventListener("click", () => {
 // totalResultResumeButton.addEventListener("click", () => {
 //   window.location.reload();
 // });
+totalResultButton.addEventListener("click", () => {
+  if (chatInput.disabled == true) {
+    return;
+  } else {
+    getTaro();
+  }
+});
+
 shareButtonReload.addEventListener("click", () => {
   window.location.reload();
 });
 
 function inputEnterKey() {
-  getTaro();
-  insertPage.style.display = "none";
-  chooseCardPage.style.display = "flex";
+  if (insertValue.value == "") {
+    return;
+  } else {
+    getTaro();
+    insertPage.style.display = "none";
+    chooseCardPage.style.display = "flex";
+  }
+}
+
+function totalResultInputEnterKey() {
+  if (totalResultInputValue.value == "") {
+    return;
+  } else {
+    getTaro();
+  }
 }
