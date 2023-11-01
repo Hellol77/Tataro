f61cf2fab5faffa37d5d4309bd705f552a516042
DongHyeonWon <dhe7700@naver.com>
2023-11-01 22:00:00 +0900

    refac : 초기 설정

---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  optimization: { minimizer: [] },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "index.html" }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "app.css" }),
+  ],
+};
