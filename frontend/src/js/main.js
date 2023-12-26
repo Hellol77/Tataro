1f07156eb14fa3b3b3d180215214ba286f6a3fbd
DongHyeonWon <dhe7700@naver.com>
2023-12-26 15:21:55 +0900

    feat:webpack

---
+++ frontend/src/js/main.js
@@ -0,0 +1,11 @@
+import './key.js';
+import './kakaoShareMessage.js';
+import './chooseCard.js';
+import './link.js';
+import './api.js';
+import './cardResult.js';
+import './download.js';
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Hello from main.js!');
+  // Add your project's initialization code or logic here
+});
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,39 @@
-const Dotenv = require("dotenv-webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // ...
-  plugins: [new Dotenv()],
+  entry: "./src/js/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: "./dist",
+    port: 8080,
+  },
 };
