c102154678be32a6405b403d2c9f12b85b209d83
DongHyeonWon <dhe7700@naver.com>
2023-12-26 15:53:21 +0900

    fix: 빌드 버그 수정

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,41 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/js/main.js",
   output: {
-    filename: "bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
-  mode: "production", // 'development' or 'production
+  mode: "development",
+  optimization: { minimizer: [] },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "img",
-            },
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
           },
-        ],
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        type: "asset/resource",
       },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
+    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "app.css" }),
   ],
-  devServer: {
-    contentBase: "./dist",
-    port: 8080,
-  },
 };
