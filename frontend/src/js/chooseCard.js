912006e84d2f2f6def70439b1eb06bf45a55ee46
DongHyeonWon <dhe7700@naver.com>
2023-12-27 16:09:51 +0900

    refac: chooseCard 리팩토링

--- frontend/src/js/chooseCard.js
+++ frontend/src/js/chooseCard.js
@@ -1,44 +1,44 @@
-export function chooseCard() {
-  let cardCount = 0;
-  const chooseCardSelect = document.getElementById("chooseCard_select");
+let cardCount = 0;
+const chooseCardSelect = document.getElementById("chooseCard_select");
+const cardBox = document.getElementById("chooseCard_cardBox");
 
-  const cardBox = document.getElementById("chooseCard_cardBox");
-  for (let i = 1; i < 79; i++) {
-    const image = document.createElement("img");
-    image.classList.add("chooseCard_card");
-    image.src = "./card-backside.png";
-    cardBox.appendChild(image);
-  }
-
-  const card = document.getElementsByClassName("chooseCard_card");
-  for (let i = 0; i < card.length; i++) {
-    card.item(i).addEventListener("click", () => {
-      if (card.item(i).classList.contains("chooseCard_card_onclick")) {
-        // 카드선택을 취소하면 선택하기 버튼을 안보이게 한다.
-        chooseCardSelect.style.display = "none";
-        card.item(i).classList.remove("chooseCard_card_onclick");
-        cardCount -= 1;
-        return;
-      }
-
-      if (cardCount >= 3) {
-        // 3개만 뽑을 수 있게한다.
-        return;
-      } else {
-        //카드 뽑기
-        cardCount += 1;
-        if (cardCount == 3) {
-          // 만약 3번째 카드를 선택하는 클릭일때 선택하기 버튼을 보이게 한다.
-          chooseCardSelect.style.display = "flex";
-        }
-        card.item(i).classList.add("chooseCard_card_onclick");
-      }
-    });
-  }
-
-  if (cardCount == 3) {
-    chooseCardSelect.style.display = "flex";
-  } else {
-    chooseCardSelect.style.display = "none";
-  }
+function createCard() {
+  const image = document.createElement("img");
+  image.classList.add("chooseCard_card");
+  image.src = "./card-backside.png";
+  return image;
 }
+
+function toggleCardSelection(card) {
+  if (card.classList.contains("chooseCard_card_onclick")) {
+    // Deselect card
+    chooseCardSelect.style.display = "none";
+    card.classList.remove("chooseCard_card_onclick");
+    cardCount -= 1;
+  } else {
+    // Select card
+    if (cardCount < 3) {
+      cardCount += 1;
+      card.classList.add("chooseCard_card_onclick");
+    }
+  }
+
+  chooseCardSelect.style.display = cardCount === 3 ? "flex" : "none";
+  console.log(cardCount);
+}
+
+function handleCardClick(event) {
+  const clickedCard = event.target;
+  toggleCardSelection(clickedCard);
+}
+
+for (let i = 1; i < 79; i++) {
+  const card = createCard();
+  cardBox.appendChild(card);
+}
+
+const cards = document.querySelectorAll(".chooseCard_card");
+
+cards.forEach(card => {
+  card.addEventListener("click", handleCardClick);
+});
